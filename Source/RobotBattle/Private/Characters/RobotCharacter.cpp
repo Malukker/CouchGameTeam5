362354e6cb6adaacdb20eed98db5b2d63f743f60
tevadu52 <tevafoug@gmail.com>
2025-11-05 14:04:32 +0100

    [UPDATE] some change

--- Source/RobotBattle/Private/Characters/RobotCharacter.cpp
+++ Source/RobotBattle/Private/Characters/RobotCharacter.cpp
@@ -9,7 +9,9 @@
 #include "Characters/RobotCharacterInputData.h"
 #include "Characters/RobotCharacterPositionEnum.h"
 #include "Characters/RobotCharacterSettings.h"
+#include "GameFramework/GameModeBase.h"
 #include "Kismet/GameplayStatics.h"
+#include "UI/MenuPauseRobotBattle.h"
 
 // Sets default values
 ARobotCharacter::ARobotCharacter()
@@ -179,6 +181,9 @@ void ARobotCharacter::OnInputPause(const FInputActionValue& InputActionValue)
 	FInputModeGameAndUI InputMode;
 	PlayerController->SetInputMode(InputMode);
 	UGameplayStatics::SetGamePaused(GetWorld(), true);
+
+	//HUDGameplay HUD = Cast<HUDGameplay>(UGameplayStatics::GetGameMode(GetWorld())->HUDClass);
+	//HUD->SetPause();
 }
 
 void ARobotCharacter::BindInputAndActions(UEnhancedInputComponent* EnhancedInputComponent) {
