e9abf1b3dc839a1100182fa956a00c15eb9d2632
elilanrayan <65083041+elilanrayan@users.noreply.github.com>
2025-11-05 23:41:33 +0100

    [MOD] settings slow motion

--- Source/RobotBattle/Public/Arena/ArenaSettings.h
+++ Source/RobotBattle/Public/Arena/ArenaSettings.h
@@ -22,7 +22,7 @@ public:
 	UPROPERTY(Config, EditAnywhere, Category="Characters")
 	TMap<ERobotID ,TSubclassOf<ARobotCharacter>> RobotCharacterDownClass;
 
-	UPROPERTY(Config, EditAnywhere, Category="SlowMotion",meta=(ClampMin="0.1", ClampMax="1.0", UIMin="0.1", UIMax="1.0"))
+	UPROPERTY(Config, EditAnywhere, Category="SlowMotion",meta=(ClampMin="0.1", UIMin="0.1"))
 	float SlowMotionDuration = 0.5f;
 
 	UPROPERTY(Config, EditAnywhere, Category="SlowMotion",meta=(ClampMin="0.1", ClampMax="1.0", UIMin="0.1", UIMax="1.0"))
