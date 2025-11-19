0f07c2cf4dd8e31abb52c91470bc3ca157449d8e
elilanrayan <65083041+elilanrayan@users.noreply.github.com>
2025-11-19 10:20:19 +0100

    [UPDATE] optimizing

--- Source/RobotBattle/Private/Match/RobotGameInstance.cpp
+++ Source/RobotBattle/Private/Match/RobotGameInstance.cpp
@@ -46,8 +46,11 @@ void URobotGameInstance::AddControllerOnList(EInputDeviceConnectionState NewConn
 		Controllers.Remove(InputDeviceId.GetId());
 	}
 	//OnInputChange(InputDeviceId.GetId(),NewConnectionState);
-	ASelectRobotActor* Actor = Cast<ASelectRobotActor>(UGameplayStatics::GetActorOfClass(GetWorld(), ASelectRobotActor::StaticClass()));
-	 Actor->WidgetTest->ControllerStateChange.Execute(NewConnectionState,InputDeviceId);
+	if (ASelectRobotActor* Actor = Cast<ASelectRobotActor>(UGameplayStatics::GetActorOfClass(GetWorld(), ASelectRobotActor::StaticClass())))
+	{
+		Actor->WidgetTest->ControllerStateChange.Execute(NewConnectionState,InputDeviceId);
+	}
+	
 	
 }
 
