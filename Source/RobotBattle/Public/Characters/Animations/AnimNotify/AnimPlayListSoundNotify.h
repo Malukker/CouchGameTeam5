bcefdad8451c5c4911760e8b932b64a506e5469a
elilanrayan <65083041+elilanrayan@users.noreply.github.com>
2025-12-01 16:00:59 +0100

    [ADD] notify sound

---
+++ Source/RobotBattle/Public/Characters/Animations/AnimNotify/AnimPlayListSoundNotify.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify_PlaySound.h"
+#include "AnimPlayListSoundNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTBATTLE_API UAnimPlayListSoundNotify : public UAnimNotify_PlaySound
+{
+	GENERATED_BODY()
+};
