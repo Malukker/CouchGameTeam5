202b319f2d28a9387521603958e00c61f688915f
elilanrayan <65083041+elilanrayan@users.noreply.github.com>
2025-12-02 11:11:08 +0100

    [ADD] data asset & notify sound

--- Source/RobotBattle/Private/Characters/Animations/AnimNotify/AnimPlayListSoundNotify.cpp
+++ Source/RobotBattle/Private/Characters/Animations/AnimNotify/AnimPlayListSoundNotify.cpp
@@ -2,3 +2,22 @@
 
 
 #include "Characters/Animations/AnimNotify/AnimPlayListSoundNotify.h"
+
+
+#include "Sounds/SoundListData.h"
+
+void UAnimPlayListSoundNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                      const FAnimNotifyEventReference& EventReference)
+{
+	Sound = GetSoundFromData();
+
+	Super::Notify(MeshComp, Animation, EventReference);
+}
+
+USoundBase* UAnimPlayListSoundNotify::GetSoundFromData() const
+{
+	if (SoundList == nullptr)
+		return nullptr;
+	SoundList->LoadConfig();
+	return SoundList->Sounds[FMath::RandRange(0, SoundList->Sounds.Num() - 1)];
+}
--- Source/RobotBattle/Public/Characters/Animations/AnimNotify/AnimPlayListSoundNotify.h
+++ Source/RobotBattle/Public/Characters/Animations/AnimNotify/AnimPlayListSoundNotify.h
@@ -6,6 +6,7 @@
 #include "Animation/AnimNotifies/AnimNotify_PlaySound.h"
 #include "AnimPlayListSoundNotify.generated.h"
 
+class USoundListData;
 /**
  * 
  */
@@ -13,4 +14,13 @@ UCLASS()
 class ROBOTBATTLE_API UAnimPlayListSoundNotify : public UAnimNotify_PlaySound
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundListData* SoundList;
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+
+private:
+	USoundBase* GetSoundFromData() const;
 };
