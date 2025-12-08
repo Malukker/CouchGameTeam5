a58a5dd95e05178294cb68875762d089b7e96ce0
tevadu52 <tevafoug@gmail.com>
2025-12-08 09:59:14 +0100

    [MOD] working build

--- Source/RobotBattle/Public/Match/WinManager.h
+++ Source/RobotBattle/Public/Match/WinManager.h
@@ -4,11 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "RobotGameInstance.h"
-#include "Camera/CameraSettings.h"
 #include "GameFramework/Actor.h"
 #include "WinManager.generated.h"
 
 class ARobotCharacter;
+class APlayerStart;
 enum class ERobotCharacterPositionEnum : uint8;
 
 UCLASS()
