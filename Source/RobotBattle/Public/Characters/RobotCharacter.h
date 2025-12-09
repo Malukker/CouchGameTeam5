8b6c1ad46314801372b4e8d4f8a45e42a11796ef
tevadu52 <tevafoug@gmail.com>
2025-12-09 11:32:01 +0100

    [MOD] better win

--- Source/RobotBattle/Public/Characters/RobotCharacter.h
+++ Source/RobotBattle/Public/Characters/RobotCharacter.h
@@ -213,7 +213,7 @@ protected:
 	UPROPERTY(EditAnywhere)
 	bool IsFollowable = false;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	bool IsLookingOpponent = false;
 	
 	UPROPERTY(EditAnywhere)
